2ea40b62e6b062c5fdaef00004c8b83bb2763260
Victor Maia <victormfseveriano@hotmail.com>
2023-01-09 16:20:57 -0300

    Structure done

--- hunter_bank_app/src/App.jsx
+++ hunter_bank_app/src/App.jsx
@@ -1,13 +1,33 @@
 import styles from "./style"
+import { Billing, Business, Button, CardDeal, CTA, FeedbackCard, Footer, GetStarted, Hero, Navbar, Stats, Testimonials, Clients } from "./components"
 
 const App = () =>  (
-    <nav className='bg-primary w-full overflow-hidden'>
-      <div className={ `${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={ `${styles.boxWidth}`}>
-          NavBar
-        </div>
-      </div>
-    </nav>
+    <div className='bg-primary w-full overflow-hidden'>
+      <section className={ `${styles.paddingX} ${styles.flexCenter}`}>
+        <nav className={ `${styles.boxWidth}`}>
+          <Navbar />
+        </nav>
+      </section>
+
+      <section className={`bg-primary ${styles.flexStart}`}>
+          <main className={`${styles.boxWidth}`}>
+              <Hero />
+          </main>
+      </section>
+
+      <section className={`bg-primary ${styles.flexStart}`}>
+          <section className={`${styles.boxWidth}`}>
+              <Stats />
+              <Business />
+              <Billing />
+              <CardDeal />
+              <Testimonials />
+              <Clients />
+              <CTA />
+              <Footer />
+          </section>
+      </section>
+    </div>
   )
 
 
